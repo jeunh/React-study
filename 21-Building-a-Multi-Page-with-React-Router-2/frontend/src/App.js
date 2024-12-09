4983754afdfece784462f344b078642fa6a6d76b
jeunh <gkwldms1218@gmail.com>
2024-12-09 23:35:23 +0900

    loader()

--- 21-Building-a-Multi-Page-with-React-Router-2/frontend/src/App.js
+++ 21-Building-a-Multi-Page-with-React-Router-2/frontend/src/App.js
@@ -22,7 +22,7 @@
 
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
-import EventsPage from "./pages/Events";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEventPage";
@@ -40,7 +40,11 @@ function App() {
           path: 'events', 
           element: <EventsRootLayout />, 
           children: [
-            { index: true, element: <EventsPage /> },
+            { 
+              index: true, 
+              element: <EventsPage />, 
+              loader: eventsLoader
+            },
             { path: ':eventId', element: <EventDetailPage /> },
             { path: 'new', element: <NewEventPage /> },
             { path: ':eventId/edit', element: <EditEventPage /> }
